5449c34c1fe5a4fd45a13ea8df3fe4faa3ba21d8
Blackbeyonder <54973451+Blackbeyonder@users.noreply.github.com>
2024-01-28 15:43:43 -0600

    detailScreen.dart - test with listView

--- lib/screens/detailScren.dart
+++ lib/screens/detailScren.dart
@@ -13,14 +13,62 @@ class DetailScren extends StatefulWidget {
 class _DetailScrenState extends State<DetailScren> {
   List<dynamic> dataCharacter = []; // Lista para almacenar los héroes
   String imageUrl = "";
+//[]  {}   "":""
+ List<Map<String, dynamic>> characterTest =[
+    {
+      "id": "69",
+      "name": "Batman",
+      "powerstats": {
+        "intelligence": "81",
+        "strength": "40",
+        "speed": "29",
+        "durability": "55",
+        "power": "63",
+        "combat": "90"
+      },
+      "biography": {
+        "full-name": "Terry McGinnis",
+        "alter-egos": "No alter egos found.",
+        "aliases": [
+          "Batman II",
+          "The Tomorrow Knight",
+          "The second Dark Knight",
+          "The Dark Knight of Tomorrow",
+          "Batman Beyond"
+        ],
+        "place-of-birth": "Gotham City, 25th Century",
+        "first-appearance": "Batman Beyond #1",
+        "publisher": "DC Comics",
+        "alignment": "good"
+      },
+      "appearance": {
+        "gender": "Male",
+        "race": "Human",
+        "height": [
+          "5'10",
+          "178 cm"
+        ],
+        "weight": [
+          "170 lb",
+          "77 kg"
+        ],
+        "eye-color": "Blue",
+        "hair-color": "Black"
+      },
+      "work": {
+        "occupation": "-",
+        "base": "21st Century Gotham City"
+      },
+      "connections": {
+        "group-affiliation": "Batman Family, Justice League Unlimited",
+        "relatives": "Bruce Wayne (biological father), Warren McGinnis (father, deceased), Mary McGinnis (mother), Matt McGinnis (brother)"
+      },
+      "image": {
+        "url": "https://www.superherodb.com/pictures2/portraits/10/100/10441.jpg"
+      }
+    },
+ ];
 
-List<String> data = [
-  'Superman',
-  'Batman',
-  'Wonder Woman',
-  'Spider-Man',
-  // Agrega más nombres de superhéroes según sea necesario
-];
 
   String heroName = '';
   String nameSelected = '';
@@ -89,6 +137,21 @@ List<String> data = [
                     child: CircularProgressIndicator(),
                   ),
                 ),
+              Expanded(
+                child: Container(
+                  color: Colors.blue,
+                  child: ListView.builder(
+                    itemCount: characterTest.length,
+                    itemBuilder: (context, index) {
+                        final character = characterTest[index];
+                      return ListTile(
+                        title: Text(character["name"]),
+                        // Puedes personalizar la apariencia de cada fila aquí
+                      );
+                    },
+                  ),
+                ),
+              ),
             ],
           ),
         ));
