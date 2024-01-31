1552856a948d7a21ac6880ace339a63dd20adb63
Blackbeyonder <54973451+Blackbeyonder@users.noreply.github.com>
2024-01-31 16:24:10 -0600

    Modificated: TEST SHAKE

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import '../services/superHeroeService.dart';
 import '../utils/homeScreenMethods.dart';
 import '../widgets/searchAppBar.dart';
+import 'package:sensors_plus/sensors_plus.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -15,6 +16,15 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     super.initState();
+     // Comienza a escuchar los eventos del acelerómetro
+    accelerometerEvents.listen((AccelerometerEvent event) {
+      // Si el valor absoluto de cualquiera de las componentes supera 10, se considera una sacudida
+      if (event.x.abs() > 10 || event.y.abs() > 10 || event.z.abs() > 10) {
+        // Imprime un mensaje cuando se detecta una sacudida
+        print('¡El dispositivo se está sacudiendo!');
+        updateData();
+      }
+    });
   }
 
   @override
